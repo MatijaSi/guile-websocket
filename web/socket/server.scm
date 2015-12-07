0d42d0d8085b4a387f2e81360ff4c0b6ad229229
David Thompson <dthompson2@worcester.edu>
2015-12-07 08:54:41 -0500

    Add missing "Lesser" in licensing comments.

--- web/socket/server.scm
+++ web/socket/server.scm
@@ -8,10 +8,10 @@
 ;;; published by the Free Software Foundation; either version 3 of the
 ;;; License, or (at your option) any later version.
 ;;;
-;;; Guile-websocket is distributed in the hope that it will be useful, but
-;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; Guile-websocket is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;;; General Public License for more details.
+;;; Lesser General Public License for more details.
 ;;;
 ;;; You should have received a copy of the GNU Lesser General Public
 ;;; License along with guile-websocket.  If not, see
